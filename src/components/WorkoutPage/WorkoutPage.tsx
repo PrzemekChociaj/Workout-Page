88e0ffae4da560f71cd85de68419b2993c004e65
PrzemekChociaj <111577035+PrzemekChociaj@users.noreply.github.com>
2023-07-04 14:45:59 +0200

    style :star: : styling Calendar + Workout Page

--- src/components/WorkoutPage/WorkoutPage.tsx
+++ src/components/WorkoutPage/WorkoutPage.tsx
@@ -5,13 +5,14 @@ const WorkoutPage = () => {
 	return (
 		<div className='flex absolute justify-center items-center rounded-md h-[80%] w-[60%] bottom-0 left-96 ml-32 mb-12 bg-gray-100'>
 			<Calendar />
+		
 
-			<div className='max-w-md mx-auto p-4 bg-white rounded shadow-lg'>
+			<div className='w-[60%] mx-auto h-[60%] p-4 bg-white rounded shadow-lg'>
 				<IframeResizer
-					src='https://www.example.com'
+					src='https://www.youtube.com/embed/zgfcOWuTeYA'
 					style={{ width: '100%', height: '100%' }}
 				/>
-			</div>
+			T</div>
 		</div>
 	);
 };
