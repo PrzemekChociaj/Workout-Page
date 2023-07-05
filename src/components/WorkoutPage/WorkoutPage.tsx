55d7a7d7a1d626fbd15112894c445039b44100e1
PrzemekChociaj <111577035+PrzemekChociaj@users.noreply.github.com>
2023-07-05 14:13:03 +0200

    refactor :art:

--- src/components/WorkoutPage/WorkoutPage.tsx
+++ src/components/WorkoutPage/WorkoutPage.tsx
@@ -1,18 +1,14 @@
 import IframeResizer from 'iframe-resizer-react';
 import Calendar from '../Calendar/Calendar';
-
+import { back } from '../../Data/WorkoutVideos';
 const WorkoutPage = () => {
 	return (
 		<div className='flex absolute justify-center items-center rounded-md h-[80%] w-[60%] bottom-0 left-96 ml-32 mb-12 bg-gray-100'>
 			<Calendar />
-		
 
 			<div className='w-[60%] mx-auto h-[60%] p-4 bg-white rounded shadow-lg'>
-				<IframeResizer
-					src='https://www.youtube.com/embed/zgfcOWuTeYA'
-					style={{ width: '100%', height: '100%' }}
-				/>
-			T</div>
+				<IframeResizer src={back} style={{ width: '100%', height: '100%' }} />
+			</div>
 		</div>
 	);
 };
