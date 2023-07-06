4644e096e732d12b9c5c635cad35bc8b076ae7b3
PrzemekChociaj <111577035+PrzemekChociaj@users.noreply.github.com>
2023-07-06 16:09:06 +0200

    styling :lipstick:

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -6,7 +6,7 @@ const Timer: React.FunctionComponent = () => {
 	const { count, isRunning, toggleTimer } = useTimer();
 
 	return (
-		<div className='flex flex-col items-center'>
+		<div className='flex absolute top-0 mt-6 mr-48 flex-col items-center'>
 			<div className='text-4xl font-bold mb-4'>{formatTime(count)}</div>
 			<div className='flex'>
 				<TimerButton isRunning={isRunning} toggleTimer={toggleTimer} />
