b5b36f773295db6a297ff4b0e73d4e0937351906
PrzemekChociaj <111577035+PrzemekChociaj@users.noreply.github.com>
2023-07-19 14:42:08 +0200

    Refactor :art: Updated Timer Button.

--- src/components/Features/TimerButton.tsx
+++ src/components/Features/TimerButton.tsx
@@ -1,29 +1,44 @@
-import React from 'react';
+
+
+interface TimerButtonProps {
+  isRunning: boolean;
+  toggleTimer: () => void;
+  resetTimer: () => void;
+
+}
 
 const getButtonColor = (isRunning: boolean): string =>
 	isRunning ? 'bg-red-500 hover:bg-red-700' : 'bg-blue-500 hover:bg-blue-700';
 
-const TimerButton: React.FunctionComponent<{
-	isRunning: boolean;
-	toggleTimer: () => void;
-	resetTimer: () => void;
-}> = ({ isRunning, toggleTimer, resetTimer }) => {
-	const buttonClassName: string = getButtonColor(isRunning);
+const TimerButton: React.FunctionComponent<TimerButtonProps> = ({
+  isRunning,
+  toggleTimer,
+  resetTimer
+}) => {
+  const buttonClassName: string = getButtonColor(isRunning);
 
-	return (
-		<div>
-			<button
-				className={`${buttonClassName} text-white font-bold py-2 px-4 rounded mr-2`}
-				onClick={toggleTimer}>
-				{isRunning ? 'Stop' : 'Start'}
-			</button>
-			<button
-				className='bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded'
-				onClick={resetTimer}>
-				Reset
-			</button>
-		</div>
-	);
+  return (
+    <div>
+      <button
+        className={`${buttonClassName} text-white font-bold py-2 px-4 rounded mr-2`}
+        onClick={toggleTimer}
+      >
+        {isRunning ? 'Stop' : 'Start'}
+      </button>
+      <button
+        className='bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded'
+        onClick={resetTimer}
+      >
+        Reset
+      </button>
+    </div>
+  );
 };
 
 export default TimerButton;
+
+
+
+
+
+
