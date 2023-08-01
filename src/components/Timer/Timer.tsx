d9cb81e1c01f00b154bcb7ad78ebe004394c04cf
PrzemekChociaj <111577035+PrzemekChociaj@users.noreply.github.com>
2023-08-01 16:39:45 +0200

    refactor :hammer:

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -6,7 +6,7 @@ const Timer: React.FunctionComponent = () => {
 	const { count, isRunning, toggleTimer, resetTimer } = useTimer();
 
 	return (
-		<div className='flex absolute top-0 mt-6 mr-48 flex-col items-center'>
+		<div className='flex absolute top-0 mt-6 mr-12 flex-col items-center'>
 			<div className='text-4xl font-bold mb-4'>{formatTime(count)}</div>
 			<div className='flex'>
 				<TimerButton
--- src/components/WorkoutPage/WorkoutPage.tsx
+++ src/components/WorkoutPage/WorkoutPage.tsx
@@ -7,7 +7,7 @@ const WorkoutPage: React.FunctionComponent = () => {
 	const { title, videoUrl } = useLocationState();
 
 	return (
-		<div className='flex items-center justify-center h-screen bg-[#61677A]'>
+		<div className='flex items-center justify-center mr-64 h-screen bg-[#61677A]'>
 			<div className='flex flex-col items-center w-[50%] h-[70%] bg-[#FFF6E0] rounded shadow-lg'>
 				<div className='w-full h-[100%] p-4'>
 					{videoUrl && (
